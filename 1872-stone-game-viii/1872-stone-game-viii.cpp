79bda4fe687597b32af9635eb5936320fc17fd14
MOHIT RATHWA <149175692+mohit-rathwa@users.noreply.github.com>
2026-08-24 16:35:53 +0530

    Time: 101 ms (74.82%), Space: 95.4 MB (41%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.cpp
+++ 1872-stone-game-viii/1872-stone-game-viii.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     int stoneGameVIII(vector<int>& stones) {
-        int  n = stones.size();
+        int n = stones.size();
+        vector<int> dp(n , 0);
+
         vector<int> pre(n,0);
-
         pre[0] = stones[0];
+
         for(int i=1;i<n;i++){
-            pre[i]+=(pre[i-1]+stones[i]);
+            pre[i] += (pre[i-1] + stones[i]);
         }
 
-        int dp = pre[n-1];
+        dp[n-1] = pre[n-1];
 
-        for(int i=n-2 ; i >=1 ; i--){
-            
-            dp = max(dp , pre[i]-dp );
+        for(int i=n-2 ;i>=1;i--){
+            int take = pre[i] - dp[i+1];
+            int skip = dp[i+1];
+
+            dp[i] = max(take,skip);
         }
-
-        return dp;
+        return dp[1];
 
     }
 };
