22c60dd637aa07eed42bee1bf99e523d0ddf3320
MOHIT RATHWA <149175692+mohit-rathwa@users.noreply.github.com>
2026-09-23 13:46:01 +0530

    Time: 15 ms (29.24%), Space: 12.5 MB (78.46%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -2,32 +2,33 @@ class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
         int n = nums.size();
-        sort(nums.begin(),nums.end());
-        vector<int> hash(n);
-        int maxi = 1;
-        int lst_idx = 0;
-        vector<int> dp(n,1);
 
-        for(int i=1;i<n;i++){
+        vector<int> hash(n);
+
+        sort(nums.begin(),nums.end());
+        vector<int> dp(n,1);
+        int lst = 0;
+        int maxi = 1;
+
+        for(int i=0;i<n;i++){
             hash[i] = i;
-            for(int j=0;j<i;j++){
-                if(nums[i]%nums[j]==0 && dp[i]<dp[j]+1){
-                    dp[i] = dp[j] + 1 ;
+            for(int j= 0; j < i ;j++){
+                if(nums[i]%nums[j] == 0 && dp[i]<dp[j]+1){
+                    dp[i] = dp[j] + 1;
                     hash[i] = j;
                 }
+            }
                 if(maxi < dp[i]){
                     maxi = dp[i];
-                    lst_idx = i;
+                    lst = i;
                 }
-            }
         }
-
         vector<int> tmp;
-        tmp.push_back(nums[lst_idx]);
-        while(lst_idx != hash[lst_idx]){
-            lst_idx= hash[lst_idx];
-            tmp.push_back(nums[lst_idx]);
+        while(lst != hash[lst]){
+            tmp.push_back(nums[lst]);
+            lst = hash[lst];
         }
+        tmp.push_back(nums[lst]);
         reverse(tmp.begin(),tmp.end());
 
         return tmp;
